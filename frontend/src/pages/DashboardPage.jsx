1c2292783f1cd9da83d00d43fc6e553112b36e74
adamf0 <adamilkom00@gmail.com>
2026-09-11 10:03:27 +0700

    debug

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -366,7 +366,7 @@ export const DashboardPage = ({ onNavigate, globalPeriodType = 'cutoff', onPerio
   const fetchActiveKuesioners = async () => {
     try {
       const jwt = localStorage.getItem('token') || localStorage.getItem('sso_token') || localStorage.getItem('jwt_token') || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaXAiOiIxMDQxMTAwNjUyMCIsIm5hbWEiOiJBREFNIEZVUlFPTiJ9.sign';
-      const response = await fetch('http://api-simonev-lpm.unpak.ac.id/api/v2/kuesioners/active', {
+      const response = await fetch('http://localhost:3000/api/v2/kuesioners/active', {
         headers: {
           'Authorization': `Bearer ${jwt}`,
           'Accept': 'application/json',
